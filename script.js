82877ac79427a7084f49e826e53fc269c4443723
Roland Levy <rolandjlevy@gmail.com>
2020-10-21 22:07:52 +0000

    Added helpers

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 import { Human } from './src/Human.js';
+import {
+  createElement,
+  getElement,
+  getAllElements
+} from './src/helpers.js';
 
 const ella = new Human({name:'Ella', age:'9 months', arms:2, legs:2});
 ella.move({direction:'forward'});
